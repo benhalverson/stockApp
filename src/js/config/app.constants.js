dab161aeea0d04fb5802d9436bda9b7f2506c019
benhalverson <benhalverson33@gmail.com>
2016-05-24 19:08:45 -0700

    fix

--- src/js/config/app.constants.js
+++ src/js/config/app.constants.js
@@ -5,11 +5,11 @@ const AppConstants = {
 	appName: 'CoveredCallScanner POC',
 	//api: 'https://sit369w86m7.etrade.com/webapiopt/optionAnalytics/ot/coveredCallScanner.json'
 	//api: 'https://sit369w86m7.etrade.com/webapiopt/optionAnalytics/ot/test.json'
-	api: 'http://lxdm898m7.etrade.com:4000/build/data/CoveredCallScanner.json'
+	// api: 'http://lxdm898m7.etrade.com:4000/build/data/CoveredCallScanner.json'
 	//api: 'http://lxdm898m7.etrade.com:4000/build/CoveredCallScanner.json'
 	// api: 'http://localhost:4000/data/CoveredCallScanner.json'
 
-	//api:'https://us.sit.etrade.com/webapiusr/alerts/alertinbox/ACCOUNT.json'
+	api:'https://us.sit.etrade.com/webapiusr/alerts/alertinbox/ACCOUNT.json'
 	//api: 'https://dit65w220m7.etrade.com/webapiusr/alerts/alertinbox/ACCOUNT.json'
 };
 
